fe708a3caf55f6a1aabb37a0fccca01b50ae1a42
agg00138 <agg00138@red.ujaen.es>
2024-10-18 18:24:10 +0200

    Update main.py with Logs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys, os, time
 
+from algoritmos.Alg01_Clase01_Grupo06 import GreedyAleatorio
 from algoritmos.Alg02_Clase01_Grupo06 import BusquedaLocal
 from algoritmos.Alg03_Clase01_Grupo06 import AlgoritmoTabu
 from utils.procesar_configuracion import Configuracion
 from utils.procesar_tsp import TSP
 from utils.utilidades import Utilidades
-from algoritmos.Alg01_Clase01_Grupo06 import GreedyAleatorio
+from utils.crear_logs import Logger
 
 if __name__ == "__main__":
 
@@ -31,6 +32,10 @@ if __name__ == "__main__":
     # Obtener la lista de archivos TSP desde la configuración
     archivos_tsp = params['archivos']
 
+    # Crear logs solo si params['echo'] es False (antes era 'no')
+    if not params['echo']:
+        os.makedirs('logs', exist_ok=True)
+
     # Procesar cada archivo TSP
     for archivo_tsp in archivos_tsp:
         ruta_archivo = os.path.join('data', archivo_tsp)  # Construye la ruta completa
@@ -43,25 +48,30 @@ if __name__ == "__main__":
         # print("Tour inicial:", tour_inicial)
 
         # Ejecutar el algoritmo Greedy Aleatorio con cada semilla
-        for semilla in semillas:
-            print(f"\nEjecutando Greedy Aleatorio con la semilla {semilla}:")
+        for i, semilla in enumerate(semillas, start=1):
+            log_greedy = Logger(nombre_algoritmo="greedy_aleatorio", archivo_tsp={'nombre': archivo_tsp}, semilla=semilla, num_ejecucion=i, echo=params['echo'])
+            log_greedy.registrar_evento(f"Ejecutando Greedy Aleatorio con la semilla {semilla}:")
             greedy_aleatorio = GreedyAleatorio(matriz_distancias, params)
             start_time = time.time()
-            tour, distancia_total = greedy_aleatorio.resolver(semilla)
+            tour, distancia_total = greedy_aleatorio.resolver(semilla, logger=log_greedy)
             execution_time = time.time() - start_time
-            print("Tour obtenido:", list(map(int, tour)))
-            print("Distancia total:", distancia_total, ", Tiempo:", execution_time)
+            log_greedy.registrar_evento(f"\nTour obtenido: {list(map(int, tour))}")
+            log_greedy.registrar_evento(f"Distancia total: {distancia_total}, Tiempo: {execution_time}")
+            log_greedy.cerrar_log()  # Solo cerrar si `echo` es False
 
-            print(f"\nEjecutando Búsqueda Local del mejor con la semilla {semilla}:")
+            log_bl = Logger(nombre_algoritmo="busqueda_local", archivo_tsp={'nombre': archivo_tsp}, semilla=semilla, num_ejecucion=i, echo=params['echo'])
+            log_bl.registrar_evento(f"Ejecutando Búsqueda Local del mejor con la semilla {semilla}:")
             busqueda_local = BusquedaLocal(tour, distancia_total, matriz_distancias, params)
             start_time = time.time()
-            tour_busqueda, distancia_busqueda = busqueda_local.resolver(semilla)
+            tour_busqueda, distancia_busqueda = busqueda_local.resolver(semilla, logger=log_bl)
             execution_time = time.time() - start_time
-            print("Distancia total:", distancia_busqueda, ", Tiempo:", execution_time)
+            log_bl.registrar_evento(f"\nTour obtenido: {list(map(int, tour_busqueda))}")
+            log_bl.registrar_evento(f"Distancia total: {distancia_busqueda}, Tiempo: {execution_time}")
+            log_bl.cerrar_log()  # Solo cerrar si `echo` es False
 
-            print(f"\nEjecutando Algoritmo Tabú con la semilla {semilla}:")
-            algoritmo_tabu = AlgoritmoTabu(tour, distancia_total, matriz_distancias, params)
-            start_time = time.time()
-            tour_tabu, distancia_tabu = algoritmo_tabu.resolver(semilla)
-            execution_time = time.time() - start_time
-            print("Distancia total:", distancia_tabu, ", Tiempo:", execution_time)
+            # print(f"\nEjecutando Algoritmo Tabú con la semilla {semilla}:")
+            # algoritmo_tabu = AlgoritmoTabu(tour, distancia_total, matriz_distancias, params)
+            # start_time = time.time()
+            # tour_tabu, distancia_tabu = algoritmo_tabu.resolver(semilla)
+            # execution_time = time.time() - start_time
+            # print("Distancia total:", distancia_tabu, ", Tiempo:", execution_time)
